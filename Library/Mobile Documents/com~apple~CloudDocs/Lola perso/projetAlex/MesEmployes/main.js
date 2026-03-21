3c4db757f8cbe9db611f1112ea15b3fa8898494f
lola <lola@weddingplan.fr>
2026-03-21 09:15:49 +0100

    ajout fiche technique

---
+++ Library/Mobile Documents/com~apple~CloudDocs/Lola perso/projetAlex/MesEmployes/main.js
@@ -0,0 +1,27 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js') // optionnel
+    }
+  });
+
+  win.loadFile('index.html'); // ton fichier HTML principal
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
